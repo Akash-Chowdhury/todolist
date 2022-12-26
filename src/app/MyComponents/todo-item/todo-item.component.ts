c813640c45d5b07bfe644417bc27e55e44f6c81d
Akash <chowdhuryakash950@gmail.com>
2022-12-26 17:50:47 +0530

    changes

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+ 
+  @Input() todo: Todo;
+  @Output() todoDelete: EventEmitter<Todo>= new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo>= new EventEmitter();
+  
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+  onClick(todo: Todo)
+  {
+    this.todoDelete.emit(todo);
+    console.log("onckick has been triggerd")
+  }
+  onCheckboxClick(todo: Todo)
+  {
+    this.todoCheckbox.emit(todo);
+  }
+
+}
---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit{
+  
+  todos: Todo[];
+  localItem: string | null;
+  constructor()
+  {
+    this.localItem=localStorage.getItem("todos");
+    if(this.localItem == null){
+    this.todos=[]
+    }
+    else{
+      this.todos=JSON.parse(this.localItem);
+    }
+  }
+
+  ngOnInit(): void {
+      
+  }
+  deleteTodo(todo: Todo){
+    console.log(todo);
+    const index=this.todos.indexOf(todo);
+    this.todos.splice(index,1);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  addTodo(todo: Todo){
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+  toggleTodo(todo: Todo){
+    console.log(todo);
+    const index=this.todos.indexOf(todo);
+    this.todos[index].active= !this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+}
